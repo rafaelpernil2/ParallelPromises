769862b92c93faceac17f1f819a068a663ba5a4f
Rafael Pernil <rafaelpernil@uma.es>
2019-11-15 07:37:58 +0100

    feat: Non functioning commit

--- src/js/utils/data-util.js
+++ src/js/utils/data-util.js
@@ -28,40 +28,58 @@ DataUtil.customPromiseAll = (promiseList, concurrentLimit) => __awaiter(void 0, 
         // console.timeLog(`${promise.name}`);
         promisesInProgress.push(DataUtil.concurrentPromiseExecRec(promise, awaitingPromises, results));
     }
+    // Await promises
+    for (let index = 0; index < promisesInProgress.length; index++) {
+        console.log(promiseList[index].name, yield promisesInProgress[index]);
+        ;
+        // No data processing here
+    }
+    console.log(results);
     return results;
 });
 DataUtil.concurrentPromiseExecRec = (promise, promiseList, resultsObject) => {
-    let fun;
+    let resultPromise;
     const awaitingPromiseList = promiseList;
     if (promise && promise.function) {
-        fun = promise.function();
+        resultPromise = promise.function();
     }
     else {
-        return new Error("Cannot read function of promise");
+        throw new Error("Cannot read function of promise");
     }
     // Logging
     console.log("\x1b[33m%s\x1b[0m", `INITIALIZED: ${promise.name}`);
-    // console.timeLog(`${promise.name}`);
-    fun.then(() => {
-        console.log("\x1b[32m%s\x1b[0m", `EXECUTED: ${promise.name}`);
-        // console.timeEnd(`${promise.name}`);
-    });
+    console.timeLog(`${promise.name}`);
     // If there any left promises to process...
+    // Else, simply execute
     if (awaitingPromiseList.length) {
         // The next promise is loaded and removed from promiseList
-        const nextPromise = awaitingPromiseList.shift();
-        fun.then((result) => {
+        resultPromise.then((result) => {
+            const nextPromise = awaitingPromiseList.shift();
+            console.log("\x1b[32m%s\x1b[0m", `EXECUTED: ${promise.name}`);
+            console.timeEnd(`${promise.name}`);
             // Add property to resultsObject
             resultsObject[promise.name] = result;
             // If a next promise was provided successfully, it is queued
-            if (nextPromise) { // Time logs
+            if (nextPromise) {
+                // Time logs
                 console.time(`${nextPromise.name}`);
                 console.log("\x1b[35m%s\x1b[0m", `QUEUED ${nextPromise.name} by ${promise.name}`);
-                // console.timeLog(`${nextPromise.name}`);
-                DataUtil.concurrentPromiseExecRec(nextPromise, awaitingPromiseList, resultsObject);
+                console.timeLog(`${nextPromise.name}`);
+                // Add property to resultsObject
+                resultsObject[promise.name] = result;
+                resultPromise = DataUtil.concurrentPromiseExecRec(nextPromise, awaitingPromiseList, resultsObject);
             }
         });
     }
-    // Else, simply execute
+    else {
+        resultPromise.then((result) => {
+            console.log("\x1b[32m%s\x1b[0m", `EXECUTED: ${promise.name}`);
+            console.timeEnd(`${promise.name}`);
+            // Add property to resultsObject
+            resultsObject[promise.name] = result;
+            // console.log(resultsObject);
+        });
+    }
+    return resultPromise;
 };
 //# sourceMappingURL=data-util.js.map
--- src/ts/utils/data-util.ts
+++ src/ts/utils/data-util.ts
@@ -3,7 +3,7 @@ import { IUntyped } from "../interfaces/i-untyped";
 
 export class DataUtil {
 
-    public static customPromiseAll = async (promiseList: Array<{ name: string, function: () => Promise<IUntyped> }>, concurrentLimit?: number) => {
+    public static customPromiseAll = async (promiseList: ICustomPromise[], concurrentLimit?: number) => {
         const promisesInProgress = [];
         const results = {};
 
@@ -23,45 +23,69 @@ export class DataUtil {
             promisesInProgress.push(DataUtil.concurrentPromiseExecRec(promise, awaitingPromises, results));
         }
 
+        // Await promises
+
+        for (let index = 0; index < promisesInProgress.length; index++) {
+            console.log(promiseList[index].name, await promisesInProgress[index]);;
+            // No data processing here
+        }
+
+        console.log(results);
+
         return results;
     }
 
-    private static concurrentPromiseExecRec = (promise: ICustomPromise, promiseList: ICustomPromise[], resultsObject: IUntyped) => {
-        let fun;
+    private static concurrentPromiseExecRec = (promise: ICustomPromise, promiseList: ICustomPromise[], resultsObject: IUntyped): Promise<IUntyped> => {
+        let resultPromise;
         const awaitingPromiseList = promiseList;
 
         if (promise && promise.function) {
-            fun = promise.function();
+            resultPromise = promise.function();
         } else {
-            return new Error("Cannot read function of promise");
+            throw new Error("Cannot read function of promise");
         }
 
         // Logging
         console.log("\x1b[33m%s\x1b[0m", `INITIALIZED: ${promise.name}`);
-        // console.timeLog(`${promise.name}`);
+        console.timeLog(`${promise.name}`);
 
-        fun.then(() => {
-            console.log("\x1b[32m%s\x1b[0m", `EXECUTED: ${promise.name}`);
-            // console.timeEnd(`${promise.name}`);
-        });
+
 
         // If there any left promises to process...
+        // Else, simply execute
         if (awaitingPromiseList.length) {
             // The next promise is loaded and removed from promiseList
-            const nextPromise = awaitingPromiseList.shift();
-            fun.then((result) => {
+
+            resultPromise.then((result) => {
+                const nextPromise = awaitingPromiseList.shift();
+                console.log("\x1b[32m%s\x1b[0m", `EXECUTED: ${promise.name}`);
+                console.timeEnd(`${promise.name}`);
                 // Add property to resultsObject
                 resultsObject[promise.name] = result;
                 // If a next promise was provided successfully, it is queued
-                if (nextPromise) {// Time logs
+                if (nextPromise) {
+                    // Time logs
                     console.time(`${nextPromise.name}`);
                     console.log("\x1b[35m%s\x1b[0m", `QUEUED ${nextPromise.name} by ${promise.name}`);
-                    // console.timeLog(`${nextPromise.name}`);
-
-                    DataUtil.concurrentPromiseExecRec(nextPromise, awaitingPromiseList, resultsObject);
+                    console.timeLog(`${nextPromise.name}`);
+                    // Add property to resultsObject
+                    resultsObject[promise.name] = result;
+                    resultPromise = DataUtil.concurrentPromiseExecRec(nextPromise, awaitingPromiseList, resultsObject);
                 }
             });
+        } else {
+            resultPromise.then((result) => {
+                console.log("\x1b[32m%s\x1b[0m", `EXECUTED: ${promise.name}`);
+                console.timeEnd(`${promise.name}`);
+                // Add property to resultsObject
+                resultsObject[promise.name] = result;
+                // console.log(resultsObject);
+            });
         }
-        // Else, simply execute
+
+
+
+
+        return resultPromise;
     }
 }

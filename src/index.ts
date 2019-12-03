747049e4b526a90053a9d9b3776945b6d34faf03
Rafael Pernil <rafaelpernil@uma.es>
2019-12-03 18:56:38 +0100

    fix: Fixed import order

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { ICustomPromise } from './interfaces/i-custom-promise';
 import { IAnyObject } from './interfaces/i-any-object';
+import { ICustomPromise } from './interfaces/i-custom-promise';
 import { DataUtil } from './utils/data-util';
 
 export const customPromiseAll = async (promiseList: ICustomPromise[], concurrentLimit?: number): Promise<IAnyObject> => {

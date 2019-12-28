7d3d8215b591b8879eb75c4d61ffd8aa8971f669
Rafael Pernil <rafaelpernil@uma.es>
2019-12-28 23:48:06 +0100

    fix: Fixed time testing

--- src/tests/init-test.ts
+++ src/tests/init-test.ts
@@ -4,6 +4,10 @@ import { ICustomPromise } from 'src/interfaces/i-custom-promise';
 import { customPromiseAll } from '../index';
 import { PromiseUtil } from '../utils/promise-util';
 
+const calcTotalTIme = (hrtime: number[]) => {
+  return hrtime[0] * 1e9 + hrtime[1];
+};
+
 describe('Initial test. The method customPromiseAll with a predefined set of dummy promises...', () => {
   it('should return the following object', async () => {
     const concurrentLimit = 3;
@@ -11,47 +15,47 @@ describe('Initial test. The method customPromiseAll with a predefined set of dum
     const listOfPromises: ICustomPromise[] = [
       {
         name: 'GetSomething',
-        function: PromiseUtil.generateRandomPromise(10),
+        function: PromiseUtil.generateRandomPromise(0),
         thisArg: undefined,
         args: [{ result: 'Result' }]
       },
       {
         name: 'CreateSomething',
-        function: PromiseUtil.generateRandomPromise(10),
+        function: PromiseUtil.generateRandomPromise(0),
         thisArg: undefined,
         args: [{ result: 'Result' }, { result2: 'Result' }]
       },
       {
         name: 'DeleteSomething',
-        function: PromiseUtil.generateRandomPromise(10)
+        function: PromiseUtil.generateRandomPromise(0)
       },
       {
         name: 'UpdateSomething',
-        function: PromiseUtil.generateRandomPromise(10)
+        function: PromiseUtil.generateRandomPromise(0)
       },
       {
         name: 'ExternalAPI1',
-        function: PromiseUtil.generateRandomPromise(10)
+        function: PromiseUtil.generateRandomPromise(0)
       },
       {
         name: 'ExternalAPI2',
-        function: PromiseUtil.generateRandomPromise(10)
+        function: PromiseUtil.generateRandomPromise(0)
       },
       {
         name: 'LoadJSON',
-        function: PromiseUtil.generateRandomPromise(10)
+        function: PromiseUtil.generateRandomPromise(0)
       },
       {
         name: 'LoadAuthCookie',
-        function: PromiseUtil.generateRandomPromise(10)
+        function: PromiseUtil.generateRandomPromise(0)
       },
       {
         name: 'LoadExternalLibraries',
-        function: PromiseUtil.generateRandomPromise(10)
+        function: PromiseUtil.generateRandomPromise(0)
       },
       {
         name: 'SendLog',
-        function: PromiseUtil.generateRandomPromise(10)
+        function: PromiseUtil.generateRandomPromise(0)
       }
     ];
 
@@ -96,31 +100,31 @@ describe('Concurrency limit test. Two executions of method customPromiseAll with
       },
       {
         name: 'UpdateSomething',
-        function: PromiseUtil.generateRandomPromise(10)
+        function: PromiseUtil.generateRandomPromise(1)
       },
       {
         name: 'ExternalAPI1',
-        function: PromiseUtil.generateRandomPromise(10)
+        function: PromiseUtil.generateRandomPromise(1)
       },
       {
         name: 'ExternalAPI2',
-        function: PromiseUtil.generateRandomPromise(10)
+        function: PromiseUtil.generateRandomPromise(1)
       },
       {
         name: 'LoadJSON',
-        function: PromiseUtil.generateRandomPromise(10)
+        function: PromiseUtil.generateRandomPromise(1)
       },
       {
         name: 'LoadAuthCookie',
-        function: PromiseUtil.generateRandomPromise(10)
+        function: PromiseUtil.generateRandomPromise(1)
       },
       {
         name: 'LoadExternalLibraries',
-        function: PromiseUtil.generateRandomPromise(10)
+        function: PromiseUtil.generateRandomPromise(1)
       },
       {
         name: 'SendLog',
-        function: PromiseUtil.generateRandomPromise(10)
+        function: PromiseUtil.generateRandomPromise(1)
       }
     ];
 
@@ -132,6 +136,6 @@ describe('Concurrency limit test. Two executions of method customPromiseAll with
     await customPromiseAll(listOfPromises, concurrentLimitSecond);
     const tSecond1 = process.hrtime(tSecond0);
 
-    expect(tFirst1[1]).to.above(tSecond1[1]);
+    expect(calcTotalTIme(tFirst1)).to.above(calcTotalTIme(tSecond1));
   });
 });
